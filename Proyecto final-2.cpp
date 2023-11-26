25fb6377df27c0f0a22b1974a7b957ee5ada73c8
CrizJimz <c.chrisjim@gmail.com>
2023-11-25 18:13:09 -0600

    Finalización de la historia parte 2

--- Proyecto final-2.cpp
+++ Proyecto final-2.cpp
@@ -15,12 +15,12 @@ void mostrarMenu(){
     cout << "5. Salir" << endl;
 }
 
-void numAletorio(){
-	srand(time(NULL));
-	int numeroAletorio=rand()%20+1;
-}
+
 
 int main(){
+	
+	srand(time(NULL));
+	int numeroAletorio=rand()%20+1;
 	
 	int opcMenu;
 	int idioma=1;
@@ -33,13 +33,19 @@ int main(){
 	int opcion4;
 	int opcion5;
 	
-	int peregrinos=100;
-	int vida=25;
-	int felicidad=10;
 	
 	system("cls");
 	
 	do{
+		
+	int peregrinos=100;
+	int vida=25;
+	int felicidad=10;
+	
+	int florMedicinal=0;
+	int plantaExtrana=0;
+	int mineralesPreciosos=0;
+		
 	mostrarMenu();
 	cout<<"\n---Selecciona una opcion";
 	cout<<endl<<">> ";
@@ -95,6 +101,18 @@ int main(){
     		system("cls");
 			break;
 			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		case 1:
 			system("cls");
 			cout<<"Era el amanecer de una nueva era en las tierras del Anahuac. Aztlan, la legendaria tierra de \n";
@@ -136,8 +154,10 @@ int main(){
 				case 1:
 					cout<<"Tu grupo y tu van a buscar materiales cerca de la zona y logran encontrar\n";
 					cout<<"fibra y algunos trozos de madera para poder pasar, ademas de:\n\n";
-					cout<<"2-flores medicinales\n";
-					cout<<"1-flor extrana\n\n";
+					cout<<"2-Flores medicinales\n";
+					cout<<"1-Planta extrana\n\n";
+					plantaExtrana=plantaExtrana+1;
+					florMedicinal=florMedicinal+2;
 					cout<<"Sin embargo a la hora de cruzar una serpiente de agua te muerde y\n";
 					cout<<"pierdes -7 de vida y 8 peregrinos cayeron en el rio \n\n";
 					vida=vida-7;
@@ -152,10 +172,13 @@ int main(){
 					cout<<"Decides que es mejor buscar una ruta alternativa a pesar de ser mqs larga\n";
 					cout<<"por lo que despues de dia y medio logran encontrar otra ruta y continuan\n";
 					cout<<"con su viaje, sin embargo los peregrinos sienten molestia. \n\n";
-					felicidad=felicidad-2;
+					felicidad=felicidad-4;
 					system("pause"); 
 					break;
+				
+				default:
 					
+					break;
 					
 			}
 			}while(opcion1==3);
@@ -165,9 +188,92 @@ int main(){
 		cout<<"==== El viaje lleva a los peregrinos a traves de un implacable     ==== \n";
 		cout<<"==== desierto. La sed y el cansancio comienzan a afectar al grupo. ==== \n";
 		cout<<"==== Tres opciones se presentan: ====\n\n";
-		cout<<"1. Buscar oasis y recursos en el desierto.\n";
-		cout<<"2. Establecer un ritmo más lento para conservar energía.\n";
-		cout<<"3. Realizar rituales sagrados para pedir la guía de los dioses.\n";
+		cout<<"1. Buscar un oasis y recursos en el desierto.\n";
+		cout<<"2. Establecer un ritmo mas lento para conservar energia.\n";
+		cout<<"3. Seguir con el ritmo que llevan hasta salir del desierto.\n\n";
+		cout<<">> ";
+		cin>> opcion2;
+		
+		switch(opcion2){
+			case 1:
+				cout<<endl;
+				cout<<"Bajan el paso que llevan para ver si alguien puede ver un oasis o recursos \n";
+				cout<<"Por lo que despues de dias de busqueda logran encontrar un oasis lleno de recursos \n";
+				cout<<"Logran rebastecerse de alimento y de recursos en el oasis encuentran: \n\n";
+				cout<<"1-Minerales preciosos \n";
+				cout<<"2-Plantas Extranas \n\n";
+				mineralesPreciosos=mineralesPreciosos+1;
+				plantaExtrana=plantaExtrana+2;
+				cout<<"Objetos totales: \n";
+				cout<<"Flores medicinales: "<<florMedicinal<<endl;
+				cout<<"Plantas extrana: "<<plantaExtrana<<endl;
+				cout<<"Minerales preciosos: "<<mineralesPreciosos<<endl<<endl;
+				cout<<"Ademas recuperaste 6 de vida.\n";
+				vida=vida+6;
+				cout<<"Vida total: "<<vida<<endl<<endl;
+				felicidad=felicidad+3;
+				
+				cout<<"Sin embargo al hacer esto algunos peregrinos mueren de hambre o por un golpe de calor -11 peregrinos";
+				peregrinos=peregrinos-11;
+				cout<<endl<<"Peregrinos totales: "<<peregrinos;
+				break;
+				
+			case 2:
+				cout<<endl;
+				cout<<"Al bajar el ritmo logran administrar mejor sus fuerzas, sin embargo en el\n";
+				cout<<"viaje se encuentran una parvada de buitres que los atacan \n\n";
+				cout<<"Que vas a hacer?\n";
+				cout<<"1. Huir\n";
+				cout<<"2. Atacar\n";
+				cout<<">> ";
+				cin>>opcion2;
+				
+				switch(opcion2){
+					case 1:
+						cout<<endl;
+						cout<<"Logras huir, sin embargo pierdes 9 de vida y 10 peregrinos son asesinados";
+						vida=vida-9;
+						peregrinos=peregrinos-10;
+						cout<<"Vida total: "<<vida<<endl;
+						cout<<"Peregrinos: "<<peregrinos<<endl;
+						break;
+						
+					case 2:
+						cout<<endl;
+						cout<<"Decides atacar a los buitres\n";
+						numeroAletorio;
+						if(numeroAletorio<=10){
+							cout<<"Provocaste a los buitres y te quitaron 11 de vida y 18 peregrinos fueron asesinados\n\n";
+							vida=vida-11;
+							peregrinos=peregrinos-18;
+							felicidad=felicidad-4;
+							cout<<"Vida total: "<<vida<<endl;
+							cout<<"Peregrinos: "<<peregrinos<<endl;
+							system("pause"); 
+							break;
+						}
+						if(numeroAletorio>10){
+							cout<<endl;
+							cout<<"Lograste vencer a los cuervos y los usaron como alimento,sin embargo \nen la batalla murieron 3 peregrinos \n";
+							vida=20;
+							peregrinos=peregrinos-3;
+							cout<<"Vida total: "<<vida<<endl;
+							cout<<"Peregrinos: "<<peregrinos<<endl;
+							system("pause"); 
+							break;
+						}	
+					}
+			case 3:
+				cout<<endl;
+				cout<<"Siguen con el ritmo, sin embargo mucho de los peregrinos no soportaron las duras condiciones por\n";
+				cout<<"lo que muchos de ellos mueren en el trayeto. -29 Peregrinos -3 de vida\n\n";
+				vida=vida-3;
+				peregrinos=peregrinos-29;
+				cout<<"Vida total: "<<vida<<endl;
+				cout<<"Peregrinos: "<<peregrinos<<endl;
+				system("pause"); 
+				break;
+		}
 			
 		cout<<endl<<endl;
 		system("pause"); 
